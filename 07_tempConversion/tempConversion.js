c470b15c2adb6207d75283c576373df87b304897
Caio <caiolimajf1@gmail.com>
2022-07-21 16:33:20 -0300

    Done exercises and tests 06 and 07.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,18 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  if (((temp-32)*5/9) == 0) {
+    return 0;
+  } else {
+return (parseFloat(((temp-32)*5/9).toFixed(1)));
+}
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  if ((temp*9/5) + 32 == 0) {
+  return 0;
+  }
+  else {
+    return (parseFloat(((temp*9/5) + 32).toFixed(1)));
+  }
 };
 
 // Do not edit below this line
